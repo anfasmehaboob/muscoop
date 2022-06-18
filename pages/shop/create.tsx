35620d43d7973b5dbfd38faec243e6c2f64838f4
anfasmehaboob <anfas.mehaboob@gmail.com>
2022-06-19 00:00:46 +0530

    ds

--- pages/shop/create.tsx
+++ pages/shop/create.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Createshop from '../../components/Shop/Createshop'
 import styles from '../../styles/Storecreate.module.scss'
 
 
@@ -7,6 +8,7 @@ type Props = {}
 export default function shopecreate({}: Props) {
   return (
     <div className={styles.mainContainer}>
+      <Createshop />
      
     </div>
   )
